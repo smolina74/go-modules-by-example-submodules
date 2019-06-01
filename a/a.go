60aeec250880a66a13db49988bf3e83048b2fefc
Santiago Molina <smolina_74@yahoo.com>
2019-06-01 11:40:00 -0300

    Add package a

---
+++ a/a.go
@@ -0,0 +1,12 @@
+package main 
+
+import ( 
+        "github.com/smolina74/go-modules-by-example-submodules/b" 
+        "fmt" 
+) 
+
+const Name = b.Name 
+
+func main() { 
+        fmt.Println(Name) 
+} 
---
+++ a/go.mod
@@ -0,0 +1,5 @@
+module github.com/smolina74/go-modules-by-example-submodules/a
+
+go 1.12
+
+require github.com/smolina74/go-modules-by-example-submodules/b v0.1.1
